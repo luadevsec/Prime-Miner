e9eb5f4fced5547333d14dd4cf29eba513926b5b
Lunna Cipher <147001609+luadevsec@users.noreply.github.com>
2024-03-26 17:01:07 -0300

    🚀 Beta (melhoria do miner)

--- Miner.C
+++ Miner.C
@@ -1,41 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// fread e fprintf
+//<prototipos>
+int miner(unsigned long long start, unsigned long long *bloco);
+void stock(unsigned long long new_primo, FILE *src, unsigned long long *bloco, unsigned long long *max);
 
 int main(void) {
-  unsigned long int fuck = 4294967295;
-  unsigned long int primos[1000000];
-  {
-    primos[0] = 2;
-    primos[1] = 3;
-    primos[2] = 5;
-    primos[3] = 7;
-    primos[4] = 11;
+  // <variaveis>
+    unsigned int contador = 0, limiter = 0;
+  unsigned long long gen, next_primo;
+  unsigned long long max = 0;
+  unsigned long long *bloco = calloc(800000, sizeof(unsigned long long));
+  // 80.000.000 80M de 8 bytes
+  // FILE
+  FILE *primo_db;
+
+
+  
+  // loading do bloco
+  primo_db = fopen("primos.txt", "a+");
+  while(fscanf(primo_db, "%llu", (bloco + max)) > 0){
+   // printf("\nindice %llu \t valor %llu",max, *(bloco + max));
+    max++;
   }
-  unsigned long int gen = 12;
-  int truly;
-  float cache;
-  int count = 4;
-  int dis = 0;
+  max--; //evita o erro de um indice com 0 a mais.
+  fclose(primo_db);
+  gen = *(bloco + max);
+
+
+
+  // mine
 
   do {
-    truly = 1;
-    for (unsigned long int n = 0; primos[n] < gen / 2; n++) {
-      cache = gen % primos[n];
-      if (cache == 0) {
-        truly = 0;
-        break;
-      }
-    }
-    if (truly == 1) {
-      count++;
-      primos[count] = gen;
-      printf("\n%ld e primo   dis: %d   armazenado em primo[%d]", gen, dis,
-             count);
-      dis = 0;
-    } else
-      dis++;
     gen++;
+    
+    if (miner(gen, bloco)) {
+    limiter++;
+      
+      primo_db = fopen("primos.txt", "a+");
+      stock(gen, primo_db, bloco, &max);
+      fclose(primo_db);
 
-  } while (1);
+
+      if(limiter == 1000){
+        limiter = 0;
+        contador++;
+      printf("\num bloco de 1.000 novos primos minerados\narmazenados em %llu \t %d blocos minerados\n",max,contador);
+        }
+    }
+  } while (max < 80000000); // limite do meu calloc
+}
+
+//<loading>
+
+
+//<miner>
+int miner(unsigned long long start, unsigned long long *bloco) {
+  int truly = 1; // todo numero é primo até que se prove o contrario kkkk;
+  for (unsigned long long n = 0; *(bloco + n) <= start / 2; n++) {
+    if (start % *(bloco + n) == 0) {
+      truly = 0;
+      break;
+    }
+  }
+      return truly;
+}
+
+//<stock>
+
+void stock(unsigned long long new_primo, FILE *src, unsigned long long *bloco, unsigned long long *max) {
+  fprintf(src, "%llu\n", new_primo);
+  *max = *max + 1;
+  unsigned long long temp = *max;
+  *(bloco + temp) = new_primo;
 }
